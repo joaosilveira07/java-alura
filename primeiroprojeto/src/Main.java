ad58403f26cb4e4c2ab102b2aaec08856d75a93e
João Pedro <joaopedrocva21@gmail.com>
2026-06-19 20:15:51 -0300

    first commit

---
+++ primeiroprojeto/src/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+
+        for (int i = 1; i <= 5; i++){
+            System.out.println("i = " + i);
+        }
+
+        System.out.println("Concluí a aula 01!");
+    }
+}
